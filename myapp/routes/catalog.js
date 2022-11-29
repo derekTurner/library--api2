c46575309366a8cae7f3be6302624c71df2a1dc3
derekTurner <derek.turner@uws.ac.uk>
2022-11-29 02:08:40 +0000

    catalog serves

--- myapp/routes/catalog.js
+++ myapp/routes/catalog.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
 // Require controller modules.
 var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
 var genre_controller = require('../controllers/genreController');
 var book_instance_controller = require('../controllers/bookinstanceController');
+
+router.use(cors());
 
 /// BOOK ROUTES ///
 
